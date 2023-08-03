3db254e1a5cb0d4eb4d7bc519d687c2855a2f448
Ellisvelandia <eyis619l@gmail.com>
2023-08-02 23:31:01 -0500

    custom libraries

--- say.py
+++ say.py
@@ -1,6 +1,10 @@
 # import cowsay 
-# import sys
-
+import sys
 
 # if len(sys.argv) == 2:
 #     cowsay.cow("hello, " + sys.argv[1])
+
+from sayings import hello
+
+if len(sys.argv) ==2:
+    hello(sys.argv[1])
